cb4248aa036c1fe1adebc0f83985e2fdb2bf11e7
Muhammad Imam arif darmawan <muhamadimamarif225@gmail.com>
2024-12-16 21:28:15 +0700

    feat:testimonia page

--- src/pages/TestimonialPage.jsx
+++ src/pages/TestimonialPage.jsx
@@ -1,8 +1,41 @@
+import { Container, Row, Col } from "react-bootstrap";
+import { testimonial } from "../data";
+import FaqComponent from "../components/FaqComponent";
+
 const TestimonialPage = () => {
   return (
-    <div>
-      <h1>Testimonial Page</h1>
+    <div className="testimonial-page">
+      <div className="testimonial">
+        <Container>
+          <Row className="mb-5">
+            <Col>
+              <h1 className="fw-bold text-center">Semua Testimonial</h1>
+              <p className="text-center">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                Perspiciatis, alias.
+              </p>
+            </Col>
+          </Row>
+          <Row className="row-cols-lg-3 row-cols-1">
+            {testimonial.map((data) => {
+              return (
+                <Col key={data.id} className="mb-5">
+                  <p className="desc shadow-sm">{data.desc}</p>
+                  <div className="people">
+                    <img src={data.image} alt="" />
+                    <div>
+                      <h5 className="mb-1">{data.name}</h5>
+                      <p className="m-0 fw-bold">{data.skill}</p>
+                    </div>
+                  </div>
+                </Col>
+              );
+            })}
+          </Row>
+        </Container>
+      </div>
+      <FaqComponent />
     </div>
-  )
-}
-export default TestimonialPage
+  );
+};
+export default TestimonialPage;
