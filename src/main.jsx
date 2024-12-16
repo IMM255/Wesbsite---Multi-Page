7d5f66f10b5d87197eb477adea5612e3ef11ab3a
Muhammad Imam arif darmawan <muhamadimamarif225@gmail.com>
2024-12-16 22:05:59 +0700

    style: add animate style

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,8 @@ import ScrollToTop from "./components/ScrollToTop.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./dist/css/main.css";
 
+import "animate.css";
+
 import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
